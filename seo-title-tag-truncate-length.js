4754ab4a6b1e5272dadfa52a8cc4b219a381673d
Carbon Digital <47960741+carbondigitalus@users.noreply.github.com>
2022-05-23 15:44:53 -0400

    Update seo-title-tag-truncate-length.js

--- seo-title-tag-truncate-length.js
+++ seo-title-tag-truncate-length.js
@@ -6,6 +6,9 @@ if (metaTitleTag.length > 61) {
 const extractMetaTitleTag = metaTitleTag.split('|')[0];
 const updatedMetaTitleTag = extractMetaTitleTag.substring(0,36) + " | Second Right Resource";
 $('title').text(updatedMetaTitleTag);
+// ADDD OG TAGS 
+$('meta[property="og:title"]').attr('content', updatedMetaTitleTag);
+$('meta[property="og:url"]').attr('content', window.location.href);
 
 }
 }); // END document.ready
